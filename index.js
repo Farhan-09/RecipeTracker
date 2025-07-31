124b3b3c692e5c65f279571b097f11c834f83507
Farhan-09 <farhanfkkh2005@gmail.com>
2025-07-31 18:42:20 +0530

    1commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// const s = "hello world"
+// console.log(s);
+// const greetingObject = new String(s);
+
+
+// console.log(typeof greetingObject); // "object"
+// console.log(greetingObject);
+// console.log(s.length === greetingObject.length)
+
+// const arr = [1, 2, 3];
+// console.log(arr.toString()); // "1,2,3
+
+
+// const shoppingList = [1,2,3,3,4]
+
+// console.log(shoppingList.length);
+// console.log(shoppingList.length);
+
+// const people = [
+//   { name: 'John', age: 30 },
+//   { name: 'Jane', age: 25 },
+//   { name: 'Jim', age: 40 }
+// ];
+
+// for (const person of people) {
+//   console.log(` this is ${person.name} ,$
+//     {person.age}`)
+// }
+
+
+// const sparseArray = [1, , , 4];
+// console.log(sparseArray.length); // 4
+
+// console.log(sparseArray[0]);
+
+// const emptyArray = new Array(5);
+// console.log(emptyArray);
+
+y =4;
+
+const a = new Array(y);
+
+
+console.log(typeof a);
+console.log(a)
